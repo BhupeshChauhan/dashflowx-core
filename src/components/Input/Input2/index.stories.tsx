2f9d1fdc6f4f14e4aa2cf477369d38aecd91c26b
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-07-26 21:01:20 +0530

    feat: added New Input Component

---
+++ src/components/Input/Input2/index.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input2 } from '.';
+
+const meta: Meta<typeof Input2> = {
+  title: 'Element/Input2',
+  component: Input2,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    lable: 'Your Email',
+    type: 'email',
+    name: 'email',
+    id: 'email',
+    placeholder: 'name@company.com',
+  },
+};
+
+export const Fullwidth: Story = {
+  args: {
+    lable: 'Your Email',
+    type: 'email',
+    name: 'email',
+    id: 'email',
+    placeholder: 'name@company.com',
+    fullwidth: true,
+  },
+};
---
+++ src/components/Input/Input2/index.tsx
@@ -0,0 +1,122 @@
+import { cn } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentProps, forwardRef } from 'react';
+
+const input2Styles = cva([
+  'w-full',
+  'flex-1',
+  'appearence-none',
+  'placeholder:text-gray-400',
+  'focus:outline-none',
+  'bg-white',
+  'px-4',
+  'py-2',
+  'text-base',
+  'text-gray-700',
+]);
+
+interface iInput2Props {
+  fullwidth?: boolean;
+  disabled?: boolean;
+  prefixElement?: JSX.Element;
+  sufixElement?: JSX.Element;
+  sucessMsg?: String;
+  errorMsg?: String;
+  lable?: String;
+  lableClassName?: String;
+  sufixElementClassName?: string;
+  prefixElementClassName?: string;
+  input2ContainerClassName?: string;
+}
+
+type Input2Props = ComponentProps<'input'> &
+  VariantProps<typeof input2Styles> &
+  iInput2Props;
+
+export const Input2 = forwardRef<HTMLInputElement, Input2Props>(
+  (
+    {
+      className,
+      prefixElement,
+      sufixElement,
+      fullwidth,
+      disabled,
+      sucessMsg,
+      errorMsg,
+      placeholder,
+      lable,
+      lableClassName,
+      sufixElementClassName,
+      input2ContainerClassName,
+      prefixElementClassName,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div className={cn('mb-2', className)}>
+        <label
+          className={cn(
+            'block mb-2 text-sm font-medium text-gray-900 dark:text-white',
+            lable ? '' : 'hidden',
+            lableClassName
+          )}
+          htmlFor="input2Element"
+        >
+          {lable}
+        </label>
+        <div
+          className={cn(
+            'flex items-center justify-start focus-within:border-b-gray-500 relative overflow-hidden border-b-2 transition',
+            lable ? 'mt-1' : '',
+            sucessMsg
+              ? 'border-green-500 focus-within:border-green-400 focus-within:border-2'
+              : '',
+            errorMsg
+              ? 'border-red-500 focus-within:border-red-400 focus-within:border-2'
+              : '',
+            fullwidth ? ' w-full' : 'w-60',
+            input2ContainerClassName
+          )}
+        >
+          <div
+            className={cn(
+              prefixElementClassName,
+              prefixElement ? '' : 'hidden'
+            )}
+          >
+            {prefixElement}
+          </div>
+          <input
+            ref={ref}
+            id="input2Element"
+            type="text"
+            autoComplete="off"
+            placeholder={placeholder}
+            className={cn(input2Styles({ className }))}
+            {...props}
+            disabled={disabled}
+          />
+          <div
+            className={cn(sufixElementClassName, sufixElement ? '' : 'hidden')}
+          >
+            {sufixElement}
+          </div>
+        </div>
+        <p
+          className={cn(
+            'mt-2 text-sm text-green-500',
+            sucessMsg ? '' : 'hidden'
+          )}
+        >
+          {sucessMsg}
+        </p>
+        <p
+          className={cn('mt-2 text-sm text-red-500', errorMsg ? '' : 'hidden')}
+        >
+          {errorMsg}
+        </p>
+      </div>
+    );
+  }
+);
