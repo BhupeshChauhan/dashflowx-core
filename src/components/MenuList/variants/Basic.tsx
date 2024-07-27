29f1121d86215d09735614c4f4ea327b3b9133c9
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-07-27 16:16:45 +0530

    Menu List fix

--- src/components/MenuList/variants/Basic.tsx
+++ src/components/MenuList/variants/Basic.tsx
@@ -37,7 +37,7 @@ export const MenuListComp = ({
           if(!showText){
             return (
                 <Tooltip 
-                  tooltipContent={<span className={'mx-4 font-medium'}>{menu.title}</span>} 
+                  tooltipContent={<span className={'mx-4 font-medium whitespace-nowrap'}>{menu.title}</span>} 
                   tooltipTrigger={<TypographyComp
                     className={cn(
                       'flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700', linkClassName
@@ -77,7 +77,7 @@ export const MenuListComp = ({
           if(!showText){
             return (
               <Tooltip 
-                tooltipContent={<span className={'mx-4 font-medium'}>{menu.title}</span>} 
+                tooltipContent={<span className={'mx-4 font-medium whitespace-nowrap'}>{menu.title}</span>} 
                 tooltipTrigger={
                   <TypographyComp
                     className={cn(
