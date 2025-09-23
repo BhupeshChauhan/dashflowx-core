a4951e3da09ffd7eb05fac1e71ce0f0a75c4c558
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2025-09-23 23:21:45 +0530

    toast update

--- src/components/Toast/ToastProviderWrapper.tsx
+++ src/components/Toast/ToastProviderWrapper.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
-import { ToastProvider, ToastViewport } from './ToastComp';
+import { useToast } from '@/lib/use-toast';
+import { ToastProvider, ToastViewport, ToastComp, ToastTitle, ToastDescription, ToastClose } from './ToastComp';
 
 interface ToastProviderWrapperProps {
   children: React.ReactNode;
 }
 
 export const ToastProviderWrapper: React.FC<ToastProviderWrapperProps> = ({ children }) => {
+  const { toasts } = useToast();
+
   return (
     <ToastProvider>
       {children}
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <ToastComp key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </ToastComp>
+        );
+      })}
       <ToastViewport />
     </ToastProvider>
   );
